3f8c60853bad607f2e6727ec8cd3e5307936df8a
chamithsandeepa <chamithsandeepa321@gmail.com>
2024-12-28 08:17:12 +0530

    git initialized

---
+++ src/main/java/com/example/Pet_Adoption_System/PetAdoptionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pet_Adoption_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionSystemApplication.class, args);
+	}
+
+}
