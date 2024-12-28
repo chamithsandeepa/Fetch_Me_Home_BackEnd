459ec30f17a0d1990e4757a4b2a6ccf6a09a42eb
chamithsandeepa <chamithsandeepa321@gmail.com>
2024-12-28 08:38:02 +0530

    Connect to the Data Base and Create files

---
+++ src/main/java/com/example/Pet_Adoption_System/Controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.Pet_Adoption_System.Controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/example/Pet_Adoption_System/Repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.Pet_Adoption_System.Repository;
+
+public interface UserRepository {
+}
