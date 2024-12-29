5afc96e84c2ba28a1f373b69f653a78d180bceef
chamithsandeepa <chamithsandeepa321@gmail.com>
2024-12-29 07:52:12 +0530

    Developed User Register Backend.

--- src/main/java/com/example/Pet_Adoption_System/Model/Model.java
+++
@@ -1,4 +0,0 @@
-package com.example.Pet_Adoption_System.Model;
-
-public class Model {
-}
--- src/main/java/com/example/Pet_Adoption_System/Repository/UserRepository.java
+++ src/main/java/com/example/Pet_Adoption_System/Repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.Pet_Adoption_System.Repository;
 
-public interface UserRepository {
+import com.example.Pet_Adoption_System.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
 }
