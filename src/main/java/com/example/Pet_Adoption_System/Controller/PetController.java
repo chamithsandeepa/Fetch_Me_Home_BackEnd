9342b54f67520cb73960b9b30270b7b26cad9c80
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-03 08:31:21 +0530

    imported libraries to PetController.java

--- src/main/java/com/example/Pet_Adoption_System/Controller/PetController.java
+++ src/main/java/com/example/Pet_Adoption_System/Controller/PetController.java
@@ -1,4 +1,12 @@
 package com.example.Pet_Adoption_System.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.Pet_Adoption_System.Model.Pet;
+import com.example.Pet_Adoption_System.Service.PetService;
 
 public class PetController {
     
