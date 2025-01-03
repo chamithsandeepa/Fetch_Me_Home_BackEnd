10c875edd81f762d8c96a73035626c5b38c8b25a
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-03 05:34:08 +0530

    Add pet.java model

--- src/main/java/com/example/Pet_Adoption_System/Model/Pet.java
+++ src/main/java/com/example/Pet_Adoption_System/Model/Pet.java
@@ -1,5 +1,79 @@
 package com.example.Pet_Adoption_System.Model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "pets")
+@Data
+@NoArgsConstructor
 public class Pet {
-    
+    @Id
+    private Integer id;
+    private String name;
+    private String species;
+    private String breed;
+    private Integer age;
+    private String sex;
+    private String status;
+    private String description;
+
+    //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
 }
+
+
+
