10f128a9d9f8fe34c6f849ab6561330ebc1bf15b
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-03 08:21:12 +0530

    defined services in petservice file

--- src/main/java/com/example/Pet_Adoption_System/Repository/PetRepository.java
+++ src/main/java/com/example/Pet_Adoption_System/Repository/PetRepository.java
@@ -1,5 +1,14 @@
 package com.example.Pet_Adoption_System.Repository;
 
-public class PetRepository {
-    
+import com.example.Pet_Adoption_System.Model.Pet;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+@Repository
+public interface PetRepository extends MongoRepository<Pet, Integer> {
+    Pet findBySex(String sex);
+    Pet findByBreed(String breed);
 }
+
--- src/main/java/com/example/Pet_Adoption_System/Service/impl/PetServiceImpl.java
+++ src/main/java/com/example/Pet_Adoption_System/Service/impl/PetServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.Pet_Adoption_System.Model.Pet;
+import com.example.Pet_Adoption_System.Model.User;
 import com.example.Pet_Adoption_System.Repository.PetRepository;
 import com.example.Pet_Adoption_System.Service.PetService; 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,26 @@ import java.util.Optional;
 
 @Service
 public class PetServiceImpl implements PetService {
-    
+
+    @Autowired
+    private PetRepository petRepository;
+
+    @Override
+    public Pet addPet(Pet pet) {
+        // Save the pet as a document in the MongoDB collection
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public Pet getPetById(Integer id) {
+        Optional<Pet> pet = petRepository.findById(id);
+       return pet.orElse(null);
+    }
+
+    @Override
+    public List<Pet> getAllPets() {
+        // Retrieve all pet documents from the collection
+        return petRepository.findAll();
+    }
+
 }
