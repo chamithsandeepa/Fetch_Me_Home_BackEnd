6de8ad23b838c6da31e5279a0324c8562395db14
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-03 10:24:24 +0530

    working in PetController.java (2)

--- src/main/java/com/example/Pet_Adoption_System/Controller/PetController.java
+++ src/main/java/com/example/Pet_Adoption_System/Controller/PetController.java
@@ -9,6 +9,16 @@ import com.example.Pet_Adoption_System.Model.Pet;
 import com.example.Pet_Adoption_System.Service.PetService;
 
 @RestController
+@CrossOrigin
+@RequestMapping("api/pets")
 public class PetController {
-    
+
+    @Autowired
+    private PetService petService;
+
+    @GetMapping
+    public ResponseEntity<List<Pet>> getAllPets() {
+        List<Pet> pets = petService.getAllPets();
+        return ResponseEntity.ok(pets);
+    }
 }
