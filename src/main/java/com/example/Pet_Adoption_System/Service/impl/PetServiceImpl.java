3874d26a640b980ae3e83d2ff34ce67dbbea4fdb
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-03 08:04:45 +0530

    Imported libraries to PetServiceImpl.java

--- src/main/java/com/example/Pet_Adoption_System/Service/impl/PetServiceImpl.java
+++ src/main/java/com/example/Pet_Adoption_System/Service/impl/PetServiceImpl.java
@@ -1,5 +1,18 @@
 package com.example.Pet_Adoption_System.Service.impl;
 
-public class PetServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Pet_Adoption_System.Model.Pet;
+import com.example.Pet_Adoption_System.Repository.PetRepository;
+import com.example.Pet_Adoption_System.Service.PetService; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+public class PetServiceImpl implements PetService {
     
 }
