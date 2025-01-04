dec4ee7549cb831fe80dc9e7d588bca7c3ce5426
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-04 10:14:10 +0530

    Import libraries to SwaggerConfig.java

--- src/main/java/com/example/Pet_Adoption_System/Security/SwaggerConfig.java
+++ src/main/java/com/example/Pet_Adoption_System/Security/SwaggerConfig.java
@@ -2,3 +2,5 @@ package com.example.Pet_Adoption_System.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
