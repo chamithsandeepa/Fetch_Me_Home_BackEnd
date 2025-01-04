5a3d95fc4ddaa608942cac6457b1eafe0bc7cc0b
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-04 11:57:32 +0530

    Import libraries to UserDetailsServiceImpl.java

---
+++ src/main/java/com/example/Pet_Adoption_System/Security/Services/UserDetailsServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.Pet_Adoption_System.Security.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.Pet_Adoption_System.Model.User;
+import com.example.Pet_Adoption_System.Repository.UserRepository;
