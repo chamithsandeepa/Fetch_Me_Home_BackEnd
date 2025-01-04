95f1b43d06fa4ffca964c1e9693d79be805a942c
ttmagedara2001 <thulanimagedara@gmail.com>
2025-01-04 11:16:31 +0530

    Import libraries to AuthTokenFilter.java

--- src/main/java/com/example/Pet_Adoption_System/Security/JWT/AuthTokenFilter.java
+++ src/main/java/com/example/Pet_Adoption_System/Security/JWT/AuthTokenFilter.java
@@ -6,3 +6,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.Pet_Adoption_System.Security.Services.UserDetailsServiceImpl
