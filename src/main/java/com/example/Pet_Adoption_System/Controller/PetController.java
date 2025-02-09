4801538816c7e1ffac4299b269a751de12c05ffe
hirusha <hirushanaveen02@gmail.com>
2025-02-09 11:23:05 +0530

    Add the Swagger API operations of Apply Pet Controller

--- src/main/java/com/example/Pet_Adoption_System/Controller/PetController.java
+++ src/main/java/com/example/Pet_Adoption_System/Controller/PetController.java
@@ -2,6 +2,8 @@ package com.example.Pet_Adoption_System.Controller;
 
 import com.example.Pet_Adoption_System.Model.Pet;
 import com.example.Pet_Adoption_System.Service.PetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +13,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/pets")
 @CrossOrigin
+@Tag(name="PetController",description="To perform operations on Pet")
 public class PetController {
 
     @Autowired
     private PetService petService;
 
+    @Operation(
+            summary = "POST operation on Pet",
+            description = "It is used to save Pets in database"
+    )
     @PostMapping
     public ResponseEntity<Pet> addPet(@RequestBody Pet pet) {
         // Log received data for debugging
@@ -25,18 +32,30 @@ public class PetController {
         return ResponseEntity.ok(savedPet);
     }
 
+    @Operation(
+            summary = "GET operation on Pet",
+            description = "It is used to get All Pets from database"
+    )
     @GetMapping
     public ResponseEntity<List<Pet>> getAllPets() {
         List<Pet> pets = petService.getAllPets();
         return ResponseEntity.ok(pets);
     }
 
+    @Operation(
+            summary = "GET operation on Pet",
+            description = "It is used to get Pets from database"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Pet> getPetById(@PathVariable String id) {
         Pet pet = petService.getPetById(id);
         return ResponseEntity.ok(pet);
     }
 
+    @Operation(
+            summary = "DELETE operation on Pet",
+            description = "It is used to delete Pets details in database"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePet(@PathVariable String id) {
         petService.deletePet(id);
